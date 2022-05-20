36cac8039ebeb54933c622b5754bbb521fce59ab
Martijn van Houwelingen <99066439@mydavinci.nl>
2022-05-20 10:30:05 +0200

    php1.4.Even of oneven?

---
+++ php_beginner/level04/even_of_oneven.php
@@ -0,0 +1,12 @@
+<?php
+
+echo 'nummer?' . PHP_EOL;
+(int)$num1 = readline();
+
+if ($num1 % 2 == 0) {
+    echo 'een even getal' . PHP_EOL;
+} else {
+    echo 'een oneven getal' . PHP_EOL;
+}
+
+?>
