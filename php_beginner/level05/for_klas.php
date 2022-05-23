5a918679f6d4ff7501eb69e6c400f6cb26d1ea8d
Martijn van Houwelingen <99066439@mydavinci.nl>
2022-05-23 11:15:14 +0200

    php1.5.For Demacia!

---
+++ php_beginner/level05/for_klas.php
@@ -0,0 +1,12 @@
+<?php
+
+$klas = ["Sjonnie de Wiel", "Herman Kaal", "Henk de Steen", "Inge Kerkhoven", "Gert Kruiswijk"];
+
+// Schrijf hier onder je code
+for ($x = 0; $x <= count($klas) - 1; $x++) {
+    echo $klas[$x] . PHP_EOL;
+}
+
+
+
+?>
