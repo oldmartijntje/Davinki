4f08bd0b164d022eed8aa6dd4b134e8950e572f6
oldmartijntje <oldmartijntje@gmail.com>
2023-02-14 12:59:34 +0100

    ez peasy lemon squizy

--- year 2/html && css && javascript/lab 5 persoonsregister/main.js
+++ year 2/html && css && javascript/lab 5 persoonsregister/main.js
@@ -1,15 +1,19 @@
-var e = 0;
-function populate() {
-    while (true) {
-        let windowRelativeBottom = document.documentElement.getBoundingClientRect().bottom;
-        if (windowRelativeBottom > document.documentElement.clientHeight + 100) break;
+list = [{
+    "voornaam": "Piet",
+    "achternaam": "Heijn",
+    "nationaliteit": "Nederlandse",
+    "leeftijd": 47,
+    "gewicht": 86
+}];
+selected = 0;
 
-        e += 1;
-        console.log(e)
-        document.body.insertAdjacentHTML("beforeend", `<img src="rickroll-roll.gif" alt=""><img src="rickroll-roll.gif" alt=""><img src="rickroll-roll.gif" alt=""><img src="rickroll-roll.gif" alt="">`);
-    }
+document.getElementById("show").addEventListener("click", myFunction);
+
+function myFunction() {
+    document.getElementById('voornaam').innerHTML = list[selected]["voornaam"];
+    document.getElementById('achternaam').innerHTML = list[selected]["achternaam"];
+    document.getElementById('nationaliteit').innerHTML = list[selected]["nationaliteit"];
+    document.getElementById('leeftijd').innerHTML = list[selected]["leeftijd"];
+    document.getElementById('gewicht').innerHTML = list[selected]["gewicht"];
+    document.getElementById('show').hidden = true;
 }
-
-window.addEventListener('scroll', populate);
-
-populate();
